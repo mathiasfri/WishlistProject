8e0841f08316929d3e4a4a7aa33b3a69bcf489f8
Maloulou99 <baha0610@stud.kea.dk>
2023-03-30 16:52:49 +0200

    CreateWish oprettet i controller og repo

--- src/main/java/com/example/wishlistproject/repositories/WishRepository.java
+++ src/main/java/com/example/wishlistproject/repositories/WishRepository.java
@@ -1,6 +1,7 @@
 package com.example.wishlistproject.repositories;
 
 import com.example.wishlistproject.models.User;
+import com.example.wishlistproject.models.Wish;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -28,6 +29,22 @@ public class WishRepository {
             pstmt.setString(2, user.getLastName());
             pstmt.setString(3, user.getMail());
             pstmt.setString(4, user.getPassword());
+            pstmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createWish(Wish wish) {
+        try(Connection con = DriverManager.getConnection(url, user_id, user_pwd)) {
+            String SQL = "INSERT INTO wishlist(wish_title, wish_description, wish_url, wish_picture) " +
+                    "VALUES(?, ?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, wish.getTitle());
+            pstmt.setString(2, wish.getDescription());
+            pstmt.setString(3, wish.getUrl());
+            pstmt.setString(4, wish.getPicture());
             pstmt.executeUpdate();
         }
         catch (SQLException e) {
