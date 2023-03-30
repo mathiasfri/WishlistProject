d26af523f70037b52bd3352e4d8229aa8f16715e
OsamaMansour25 <osam0289@stud.kea.dk>
2023-03-30 16:24:28 +0200

    Made createUser method and endpoint for it

--- src/main/java/com/example/wishlistproject/Controllers/WishController.java
+++ src/main/java/com/example/wishlistproject/Controllers/WishController.java
@@ -1,14 +1,32 @@
 package com.example.wishlistproject.Controllers;
 
+import com.example.wishlistproject.Models.User;
 import com.example.wishlistproject.Repositories.WishRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WishController {
-    WishRepository repository = new WishRepository();
+    WishRepository repository;
 
     public WishController(WishRepository repository) {
         this.repository = repository;
     }
+    @GetMapping("/create")
+    public String createUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "createUser";
+
+    }
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user) {
+        repository.createUser(user);
+        return "";
+    }
+
 }
 
--- src/main/java/com/example/wishlistproject/Repositories/WishRepository.java
+++ src/main/java/com/example/wishlistproject/Repositories/WishRepository.java
@@ -1,12 +1,38 @@
 package com.example.wishlistproject.Repositories;
 
 import com.example.wishlistproject.Models.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @Repository()
 public class WishRepository {
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String user_id;
+    @Value("${spring.datasource.password")
+    String user_pwd;
+
 
     public void createUser(User user) {
-
+        try(Connection con = DriverManager.getConnection(url, user_id, user_pwd)) {
+            String SQL = "INSERT INTO users(first_name, last_name, user_email, user_password) " +
+                    "VALUES(?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(SQL);
+            pstmt.setString(1, user.getFirstName());
+            pstmt.setString(2, user.getLastName());
+            pstmt.setString(3, user.getMail());
+            pstmt.setString(4, user.getPassword());
+            pstmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
