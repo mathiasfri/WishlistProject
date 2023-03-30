95cc8ecdc303df8f627a2ca3ee3c1660d5e59efa
mathiasfri <mathiasfridthjof@live.dk>
2023-03-30 14:49:35 +0200

    First commit

---
+++ src/main/java/com/example/wishlistproject/User.java
@@ -0,0 +1,51 @@
+package com.example.wishlistproject;
+
+public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
---
+++ src/main/java/com/example/wishlistproject/Wish.java
@@ -0,0 +1,58 @@
+package com.example.wishlistproject;
+
+public class Wish {
+    private int id;
+    private String title;
+    private String description;
+    private String url;
+    private String picture;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
