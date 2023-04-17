4616ff55686d915101542e27d0bf30d204cdf10e
JesperZamora <113138989+JesperZamora@users.noreply.github.com>
2023-04-17 13:11:37 +0000

    corrected login redirect

--- src/main/java/com/example/wishlistproject/controller/LoginController.java
+++ src/main/java/com/example/wishlistproject/controller/LoginController.java
@@ -58,7 +58,7 @@ public class LoginController {
     public String logout(HttpSession session) {
         // invalidate session and return landing page
         session.invalidate();
-        return "redirect:/wishlish/login";
+        return "login";
     }
 }
 
